82ba7439c09cfcccd8cea4f95122d23f9f7b1fc2
Chamidilshika <chamidilshika7@gmail.com>
2026-05-07 13:47:10 +0530

    initial project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  
+  const [data,setData] = useState("");
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then(res => res.json())
+      .then(data => setData(data.message));
+  }, []);
+
+  return <h1>{data}</h1>;
+}
+
+export default App;
